80b2e04e31e25e3b07b6d24fcac125f22999d6e9
Russo-BIT <roothie.ro@gmail.com>
2023-02-16 15:04:44 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
     char Mystr[] = "\"Programming is like building a multilingual puzzle";
     
     puts(Mystr); /*writing string to stdout*/
     
-    return 0;
+    return (0);
 }
