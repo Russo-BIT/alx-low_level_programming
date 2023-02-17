18778f387fe1cda7ecb18f023d3cc56fc43b7f5b
Russo-BIT <roothie.ro@gmail.com>
2023-02-16 17:34:39 -0800

    4-6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Always return value of 0 (Successful execution)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
