e8931a54cf61522815311b354603af04cea62300
Russo-BIT <roothie.ro@gmail.com>
2023-02-17 17:52:43 -0800

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,17 @@
 
 int main(void)
 {
-	int x;
-	for (x = 0; x < 10; x++)
+	int x = 0;
+	while (x < 10)
 	{
 		putchar(x + '0');
-		if (x != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		if (x == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		x++;
 	}
+}
 	putchar('\n');
 
 	return (0);
