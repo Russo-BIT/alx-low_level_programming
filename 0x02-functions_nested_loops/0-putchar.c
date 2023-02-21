d66ebb0537787388b34466a4e1cfbf92434defb3
Russo-BIT <roothie.ro@gmail.com>
2023-02-21 13:51:53 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followd by a new line
+ * Return: Always 0 (Successful execution)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(word[c]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
