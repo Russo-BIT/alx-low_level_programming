60bb345b0257c31be8b038fd7cc79f31ddc5100f
Russo-BIT <roothie.ro@gmail.com>
2023-02-23 14:59:15 -0800

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Print numbers between 0 to 9 incl.
+ *
+ * Return: Void.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c < '10'; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
