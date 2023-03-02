6493e4ba5298a38884d4cbd09f28d9354d7a2cef
Russo-BIT <roothie.ro@gmail.com>
2023-03-02 14:43:32 -0800

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+	for (i = 0; dest[i] != '\0'; i++);
 
-	for (int j = 0; src[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
