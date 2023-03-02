dfcd8ea9d44121dab469c09f633131e456143806
Russo-BIT <roothie.ro@gmail.com>
2023-03-02 15:37:02 -0800

    task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function that concatenates two strings
+ * @src: input string
+ * @dest: input string
+ * @n: input integer
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] <= n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
