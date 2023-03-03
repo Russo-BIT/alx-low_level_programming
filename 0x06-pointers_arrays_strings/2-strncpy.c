b2f701b408d103c764d61a6fe17efa0c8ab67eee
Russo-BIT <roothie.ro@gmail.com>
2023-03-02 16:01:07 -0800

    task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @src: input string
+ * @dest: input string
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0, j = 0; dest[i] != '\0' || src[j] != '\0'; i++, j++)
+	{
+		if (dest[i] == src[j])
+		{
+			continue;
+		}
+		else
+		{
+		n = dest[i] - src[j];
+		}
+		return (n);
+	}
+	return (0);
+}
