d53eae064b4889b2e131b99f6344e927a8b4b42a
Russo-BIT <roothie.ro@gmail.com>
2023-03-09 16:14:57 -0800

    tasks

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that returns the
+ * value of x raised to the power of y
  * @x: integer
  * @y: power to raise integer to
  * Return: value of x raised to the power of y
