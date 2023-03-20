b42d3397d9c2da04f16bd15ed47977d1b7365d67
Russo-BIT <roothie.ro@gmail.com>
2023-03-20 16:35:57 -0700

    task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from
+ * main - prints the name of the
+ * file it was compiled from
  *
  * Return: Always 0 (Successful execution)
  */
